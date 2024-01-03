ba932ff82fff97fa3fc5d798d9f7e24c799de942
NoraSaffar <norasaffar8910@gmail.com>
2024-01-03 11:52:26 +0000

    enhancements: add db connection

---
+++ src/db/connection.js
@@ -0,0 +1,9 @@
+const {Seqeulize} = require("sequelize");
+
+const sequelize = new Sequelize(process.env.MYSQL_URI);
+
+sequelize.authenticate();
+
+console.log("DB connection is working");
+
+module.exports = sequelize;
